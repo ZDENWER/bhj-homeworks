c6f376a6a724052404de16520a335de48f59cfa7
ZDENWER <denwer-zelensky@mail.ru>
2023-02-02 15:15:26 +0300

    hw3 ex1 done

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,30 +1,33 @@
-const dropdownVal = document.getElementsByClassName('dropdown__value');
-const dropdownList = document.getElementsByClassName('dropdown__list');
+const dropdownVal = document.querySelectorAll('.dropdown__value');
+const dropdownLists = document.getElementsByClassName('dropdown__list');
 const dropdownItems = document.querySelectorAll('.dropdown__item');
 const dropdownLinks = document.querySelectorAll('.dropdown__link');
 
-function listOpen() {
-    dropdownList[0].classList.add('dropdown__list_active');
+function listOpen(i) {
+    dropdownLists[i].classList.add('dropdown__list_active');
 };
 
-function listClose() {
-    dropdownList[0].classList.remove('dropdown__list_active');
-}
+function listClose(i) {
+    dropdownLists[i].classList.remove('dropdown__list_active');
+};
 
-dropdownVal[0].onclick = function() {
-    
-    if (!dropdownList[0].classList.contains('dropdown__list_active')) {
-        listOpen();
-    } else {
-        listClose();
+for (let i = 0; i < dropdownVal.length; i++) {
+    dropdownVal[i].onclick = function() {
+        if (!dropdownLists[i].classList.contains('dropdown__list_active')) {
+            listOpen(i);
+        } else {
+            listClose(i);
+        }
     }
-};
+}
 
 dropdownItems.forEach(e => {
     e.onclick = function() {
-        let parentDropdown = e.closest('.dropdown');// Все хуйня, не работает
-        parentDropdown.children[1].textContent = e.children[0].textContent;//
-        listClose();
+        let parentDropdown = e.closest('.dropdown');
+        parentDropdown.children[0].textContent = e.children[0].textContent;
+        for (let i = 0; i < dropdownLists.length; i++) {
+            listClose(i);
+        }
     }
 })
 
